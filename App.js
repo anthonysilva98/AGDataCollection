d1428e210e7ede3e72ae26fda495bea778606ddf
anthonysilva98 <anthonyssilva98@gmail.com>
2022-10-03 19:26:25 -0700

    Import SearchBar into Home Screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import Mapview from "./Screens/MapScreen";
 import { StyleSheet, Text, View } from "react-native";
-
+import SearchBar from "./Src/Components/SearchBar";
 export default function App() {
   return (
     <View style={styles.container}>
       <Mapview />
+      <SearchBar />
     </View>
   );
 }
