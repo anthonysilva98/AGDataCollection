f47bc9a005ab7c4319a5b56c900e53e3e3f15b91
anthonysilva98 <anthonyssilva98@gmail.com>
2022-10-03 21:19:34 -0700

    Allows to tap on screen to dismiss keyboard

--- Screens/MapScreen/Mapview.js
+++ Screens/MapScreen/Mapview.js
@@ -1,22 +1,37 @@
-import { View, Text, Dimensions, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import MapView from "react-native-maps";
 import React from "react";
 import SearchBar from "../../Src/Components/SearchBar/SearchBar";
 const Mapview = () => {
   return (
-    <View style={styles.container}>
-      <MapView
-        initialRegion={{
-          longitude: -120.592805,
-          latitude: 37.404814,
-          latitudeDelta: 0.0025,
-          longitudeDelta: 0.001,
-        }}
-        mapType="satellite"
-        style={styles.map}
-      />
-      <SearchBar />
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        console.warn("Keyboard Dismissed");
+        Keyboard.dismiss();
+        //Dismisses keyboard by tapping on Screen instead of return
+      }}
+    >
+      <View style={styles.container}>
+        <MapView
+          initialRegion={{
+            longitude: -120.592805,
+            latitude: 37.404814,
+            latitudeDelta: 0.0025,
+            longitudeDelta: 0.001,
+          }}
+          mapType="satellite"
+          style={styles.map}
+        />
+        <SearchBar />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
